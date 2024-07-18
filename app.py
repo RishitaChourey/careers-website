332e995406572c2a06cb39e42531aac411803a8f
Rishita Chourey <choureyrishita@gmail.com>
2024-07-18 21:22:43 +0530

    Separate page added for each job position

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
-from flask import Flask, render_template,jsonify
-from database import load_jobs_from_db
+from flask import Flask, render_template,jsonify, request
+from database import load_job_from_db, load_jobs_from_db
+
 app=Flask(__name__)#object of class
-
-
 
 @app.route("/")
 def first():
@@ -14,5 +13,17 @@ def first():
 def list_jobs():
   jobs_list=load_jobs_from_db()
   return jsonify(jobs_list)
+
+
+
+@app.route("/job/<int:id>")
+def show_job(id):
+  job=load_job_from_db(id)
+  if not job:
+    return "Not Found",404
+  
+  job_dict = job._asdict()
+  return render_template('jobpage.html',job=job_dict)
+
 if __name__=="__main__":
   app.run(host='0.0.0.0',debug=True)
--- database.py
+++ database.py
@@ -2,25 +2,48 @@ from sqlalchemy import create_engine, text
 from dotenv import load_dotenv
 import os
 
-
 load_dotenv()
 connection_string=os.getenv('DB_CONNECTION_STRING')
-engine = create_engine(connection_string)
+
+
+db_host = os.getenv("DB_HOST")
+db_port = os.getenv("DB_PORT")
+db_user = os.getenv("DB_USER")
+db_password = os.getenv("DB_PASSWORD")
+db_name = os.getenv("DB_NAME")
+
+timeout = 10
+
+engine = create_engine(f"mysql+pymysql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}")
+
+
 
 def load_jobs_from_db():
   with engine.connect() as conn:
-    # Execute the query and store the result in a variable
     result = conn.execute(text("select * from jobs"))
-    
     result_all=result.all()
     #print(type(result.all()))
     #result.all() is of type list
     jobs = []
-    
-    # Iterate over each row
     for row in result_all:
       # Convert each row to a dictionary using the _mapping attribute
       row_dict = dict(row._mapping)
       jobs.append(row_dict)
     return jobs
+
+
+
+def load_job_from_db(id):
+  with engine.connect() as conn:
+    result = conn.execute(
+       text(f"SELECT * FROM jobs WHERE id={id}")
+      )
+    rows = []
+    for row in result.all():
+      rows.append(row._mapping)
+    if len(rows) == 0:
+      return None
+    else:
+      return row
+    
 
